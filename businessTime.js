52ca95f63f69c6f5c1418ac974a1fc8d2a923d1d
Abel Ponce <vnponce8@gmail.com>
2019-07-13 13:55:54 -0500

    Validating that it was passing a valid JS number

--- businessTime.js
+++ businessTime.js
@@ -1,16 +1,18 @@
-export default businessTime;
+export default addBusinessTime;
 
-function businessTime(holiday, time, duration) {
+function addBusinessTime(holiday, time, duration) {
     if(holiday === undefined || holiday.constructor.name !== 'Object') return 'holiday must exist as Object';
     if(holiday.start === undefined || holiday.start.constructor.name !== 'Date') return 'Start must exist as Date';    
     if(holiday.end === undefined || holiday.end.constructor.name !== 'Date') return 'End must exist as Date';
     if(time === undefined || time.constructor.name !== 'Date') return 'Time must exist as Date';
     if(duration === undefined || duration.constructor.name !== 'Number') return 'Duration must exist as Number';
+    if(duration > Number.MAX_SAFE_INTEGER || duration < Number.MIN_SAFE_INTEGER) return 'Duration is not a permited JS number';
 
     const { start, end } = holiday;
-    if(time < start && addTime(time, duration) > start){
+    const endTime = addTime(time, duration);
+    if(time < start && endTime > start){
         console.log('time before start and time+duration bigger than start')
-        const overlapingTime = diffBetweenDates(addTime(time, duration), start);
+        const overlapingTime = diffBetweenDates(endTime, start);
         return addTime(end, overlapingTime);
     }
     else if(time >= start && time < end) {
@@ -18,25 +20,18 @@ function businessTime(holiday, time, duration) {
         if(duration > 0) return addTime(end, duration);
         return addTime(start, duration);
     }
-    return addTime(time, duration);
+    return endTime;
 }
 
 // @todo create test
 const addTime = (date, duration) => {
-    // loggerTime('addTime, date => ', date);
-    // console.log('addTime, duration =>', duration);
-    // @todo do a best approach and test it. Cloning date.
     const d = new Date(date.getTime());
     d.setSeconds(d.getSeconds() + duration);
     return d;
 }
 
 // @todo create test
-const diffBetweenDates = (first, second) => {
-    // loggerTime('diff, first => ', first);
-    // loggerTime('diff, second =>', second);
-    return (first.getTime() - second.getTime()) / 1000;
-}
+const diffBetweenDates = (first, second) => (first.getTime() - second.getTime()) / 1000;
 
 // Timezone it was killing me when I was debuggin time
 const dateWithoutTimeZone = (date) => {
--- businessTime.test.js
+++ businessTime.test.js
@@ -1,4 +1,4 @@
-import businessTime from './businessTime';
+import addBusinessTime from './businessTime';
 
 // Christmas 2019, 9pm Dec 24th to 9pm Dec 25th
 const holiday = {
@@ -8,94 +8,106 @@ const holiday = {
 
 test('Dec 1st plus 1hr must return Dec 1st at 1:00', () => {
     const expected = new Date('2019-12-01T01:00:00');
-    expect(businessTime(holiday, new Date('2019-12-01T00:00:00'), 60 * 60)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-01T00:00:00'), 60 * 60)).toEqual(expected);
 });
 
 test('Dec 1st subs 1s must return Nov 30 at 23:59:59', () => {
     const expected = new Date('2019-11-30T23:59:59');
-    expect(businessTime(holiday, new Date('2019-12-01T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-01T00:00:00'), -1)).toEqual(expected);
 });
 
 
 test('Dec 24 at 21:00 plus 1s must return Dec 25 at 21:00:01', () => {
     const expected = new Date('2019-12-25T21:00:01');
-    expect(businessTime(holiday, new Date('2019-12-24T21:00:00'), 1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-24T21:00:00'), 1)).toEqual(expected);
 });
 
 test('Dec 24 at 21:00 subs 1s must return Dec 24 at 20:59:59', () => {
     const expected = new Date('2019-12-24T20:59:59');
-    expect(businessTime(holiday, new Date('2019-12-24T21:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-24T21:00:00'), -1)).toEqual(expected);
 });
 
 
 test('Dec 24 at 20:30 plus 1h must return Dec 25 at 21:30:00', () => {
     const expected = new Date('2019-12-25T21:30:00');
-    expect(businessTime(holiday, new Date('2019-12-24T20:30:00'), 60 * 60)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-24T20:30:00'), 60 * 60)).toEqual(expected);
 });
 
 test('Dec 24 at 20:30 subs 1s must return Dec 24 at 20:29:59', () => {
     const expected = new Date('2019-12-24T20:29:59');
-    expect(businessTime(holiday, new Date('2019-12-24T20:30:00'), -1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-24T20:30:00'), -1)).toEqual(expected);
 });
 
 
 test('Dec 25 at 00:00 plus 1s must return Dec 25 at 21:00:01', () => {
     const expected = new Date('2019-12-25T21:00:01');
-    expect(businessTime(holiday, new Date('2019-12-25T00:00:00'), 1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-25T00:00:00'), 1)).toEqual(expected);
 });
 
 test('Dec 25 at 00:00 subs 1s must return Dec 24 at 20:59:59', () => {
     const expected = new Date('2019-12-24T20:59:59');
-    expect(businessTime(holiday, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
 });
 
 // @todo test validate holiday object, start is date and end is date, also second arg must be date object and durations must be a integer
 test('Holiday not exist must return an error', () => {
     const expected = 'holiday must exist as Object';
-    expect(businessTime()).toEqual(expected);
+    expect(addBusinessTime()).toEqual(expected);
 });
 
 test('Wrong holiday format must return an error', () => {
     const expected = 'holiday must exist as Object';
-    expect(businessTime([])).toEqual(expected);
+    expect(addBusinessTime([])).toEqual(expected);
 });
 
 test('Start property not exist must return an error', () => {
     const expected = 'Start must exist as Date';
-    expect(businessTime({}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime({}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
 });
 
 test('Start property is not a Date must return an error', () => {
     const expected = 'Start must exist as Date';
-    expect(businessTime({start: 'not-date'}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime({start: 'not-date'}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
 });
 
 test('End property not exist must return an error', () => {
     const expected = 'End must exist as Date';
-    expect(businessTime({start: new Date()}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime({start: new Date()}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
 });
 
 test('End property is not a Date must return an error', () => {
     const expected = 'End must exist as Date';
-    expect(businessTime({start: new Date(), end: 'not-date'}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
+    expect(addBusinessTime({start: new Date(), end: 'not-date'}, new Date('2019-12-25T00:00:00'), -1)).toEqual(expected);
 });
 
 test('Time property not exist must return an error', () => {
     const expected = 'Time must exist as Date';
-    expect(businessTime(holiday)).toEqual(expected);
+    expect(addBusinessTime(holiday)).toEqual(expected);
 });
 
 test('Time property is not a Date must return an error', () => {
     const expected = 'Time must exist as Date';
-    expect(businessTime(holiday, 'not-date')).toEqual(expected);
+    expect(addBusinessTime(holiday, 'not-date')).toEqual(expected);
 });
 
 test('Duration property not exist must return an error', () => {
     const expected = 'Duration must exist as Number';
-    expect(businessTime(holiday, new Date('2019-12-25T00:00:00'))).toEqual(expected);
+    expect(addBusinessTime(holiday, new Date('2019-12-25T00:00:00'))).toEqual(expected);
 });
 
 test('Duration property is not a Date must return an error', () => {
     const expected = 'Duration must exist as Number';
-    expect(businessTime(holiday,new Date('2019-12-25T00:00:00'), 'not-int')).toEqual(expected);
+    expect(addBusinessTime(holiday,new Date('2019-12-25T00:00:00'), 'not-int')).toEqual(expected);
+});
+
+test('Duration property is not bigger than JS permited', () => {
+    const expected = 'Duration is not a permited JS number';
+    const biggest = Number.MAX_SAFE_INTEGER + 1;
+    expect(addBusinessTime(holiday,new Date('2019-12-25T00:00:00'), biggest)).toEqual(expected);
+});
+
+test('Duration property is not a Date must return an error', () => {
+    const expected = 'Duration is not a permited JS number';
+    const smallest = Number.MIN_SAFE_INTEGER - 1;
+    expect(addBusinessTime(holiday,new Date('2019-12-25T00:00:00'), smallest)).toEqual(expected);
 });
