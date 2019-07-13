66f4cc6df5115460737b6485ad13efa39a3421ed
Abel Ponce <vnponce8@gmail.com>
2019-07-13 11:53:56 -0500

    passing first two with -1 as duration

--- businessTime.js
+++ businessTime.js
@@ -2,19 +2,30 @@ export default businessTime;
 
 function businessTime(holiday, time, duration) {
     const { start, end } = holiday;
+    loggerTime('start =>', start);
+    loggerTime('end =>', end);
+    loggerTime('time =>', time);
+    console.log('duration =>', duration);
     if(time < start && addTime(time, duration) > start){
+        console.log('time before start and time+duration bigger than start')
         const overlapingTime = diffBetweenDates(addTime(time, duration), start);
         return addTime(end, overlapingTime);
     }
     else if(time >= start && time < end) {
+        console.log('time is between start and end holiday, so add to end holiday duration');
         if(duration > 0) return addTime(end, duration);
         return addTime(start, duration);
     }
-    return addTime(time, duration);
+    console.log('else');
+    const response = addTime(time, duration);
+    loggerTime('response =>', response);
+    return response;
 }
 
 // @todo create test
 function addTime(date, duration) {
+    loggerTime('addTime, date => ', date);
+    console.log('addTime, duration =>', duration);
     // @todo best this approach and test it. Cloning date.
     const d = new Date(date.getTime());
     d.setSeconds(d.getSeconds() + duration);
@@ -23,15 +34,19 @@ function addTime(date, duration) {
 
 // @todo create test
 function diffBetweenDates(first, second) {
+    // loggerTime('diff, first => ', first);
+    // loggerTime('diff, second =>', second);
     return (first.getTime() - second.getTime()) / 1000;
 }
 
 // Timezone it was killing me when I was debuggin time
 function dateWithoutTimeZone(date) {
+    console.log('dwtz, date =>', date);
     const userTimezoneOffset = date.getTimezoneOffset() * 60000;
     return new Date(date.getTime() - userTimezoneOffset);
 }
 
 function loggerTime(string = '', date) {
+    console.log(date);
     console.log(string, dateWithoutTimeZone(date));
 }
--- businessTime.test.js
+++ businessTime.test.js
@@ -11,10 +11,21 @@ test('Dec 1st plus 1hr must return Dec 1st at 1:00', () => {
     expect(businessTime(holiday, new Date('2019-12-01T00:00:00'), 60 * 60)).toEqual(expected);
 });
 
+test('Dec 1st subs 1s must return Nov 30 at 23:59:59', () => {
+    const expected = new Date('2019-11-30T23:59:59');
+    expect(businessTime(holiday, new Date('2019-12-01T00:00:00'), -1)).toEqual(expected);
+});
+
 test('Dec 24 at 21:00 plus 1s must return Dec 25 at 21:00:01', () => {
     const expected = new Date('2019-12-25T21:00:01');
     expect(businessTime(holiday, new Date('2019-12-24T21:00:00'), 1)).toEqual(expected);
 });
+
+test('Dec 24 at 21:00 1s must return Dec 24 at 20:59:59', () => {
+    const expected = new Date('2019-12-24T20:59:59');
+    expect(businessTime(holiday, new Date('2019-12-24T21:00:00'), -1)).toEqual(expected);
+});
+
 
 test('Dec 24 at 20:30 plus 1h must return Dec 25 at 21:30:00', () => {
     const expected = new Date('2019-12-25T21:30:00');
